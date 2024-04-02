4a960ed2e889098633651234d977435bcbac60b7
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2024-04-02 11:31:14 +0200

    correct bathymetry

--- src/ocean_bathymetry.jl
+++ src/ocean_bathymetry.jl
@@ -24,7 +24,7 @@ function random_bathymetry()
     Nx = grid_size[1]
     Ny = grid_size[2]
 
-    bottom = rand(Nx, Ny)
+    bottom = - rand(Nx, Ny) .* 6000
     
     # With a random bathymetry roughly 50% of the 
     # domain should be immersed. The ocean has 42%
