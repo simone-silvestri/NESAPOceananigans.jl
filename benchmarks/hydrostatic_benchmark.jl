4b2e94c2853b63f0cf74f02225e20d06c799da63
Simone Silvestri <silvestri.simone0@gmail.com>
2024-04-11 08:54:14 -0700

    experiments:

--- benchmarks/hydrostatic_benchmark.jl
+++ benchmarks/hydrostatic_benchmark.jl
@@ -6,7 +6,9 @@ arch = GPU()
 # By default the problem size is 1440×600×50 which corresponds to about 
 # 10 GB for the full ocean testcase which should saturate the GPU compute
 # We can change it with 
-set_problem_size!(10, 10, 10)
+# set_problem_size!(10, 10, 10)
+
+case = parse(Int, get(ENV, "CASE", "0"))
 
 # The choice for a bottom height for the immersed boundary is
 # `nothing`             -> flat bottom
@@ -21,14 +23,20 @@ set_problem_size!(10, 10, 10)
 # WENO(order = 5)         -> a standard "average-performing" tracer advection scheme
 # default                 -> the best compromise between stability and accuracy. Very expensive
 
+if case == 0
+
 # Tracer advection benchmark
 @show trial1 = run_model_benchmark!(tracer_kernel_test, arch;
                                     use_benchmarktools = false)
+
+elseif case == 1
 
 # Simple advection benchmark
 @show trial2 = run_model_benchmark!(tracer_kernel_test, arch;
                                     use_benchmarktools = false,
                                     tracer_advection = Centered())
+
+elseif case == 2
 
 # Simple advection benchmark with a bathymetry
 @show trial3 = run_model_benchmark!(tracer_kernel_test, arch;
@@ -44,14 +52,20 @@ set_problem_size!(10, 10, 10)
 # WENO(order = 5)         -> a standard "average-performing" momentum advection scheme
 # default                 -> the best compromise between stability and accuracy. Very expensive
 
+elseif case == 3
+
 # Momentum advection benchmark
 @show trial1 = run_model_benchmark!(momentum_kernel_test, arch;
                                     use_benchmarktools = false)
+
+elseif case == 4
 
 # Simple advection benchmark
 @show trial2 = run_model_benchmark!(momentum_kernel_test, arch;
                                     use_benchmarktools = false,
                                     momentum_advection = VectorInvariant())
+
+elseif case == 5
 
 @show trial3 = run_model_benchmark!(momentum_kernel_test, arch;
                                     use_benchmarktools = false,
@@ -66,10 +80,14 @@ set_problem_size!(10, 10, 10)
 # WENO(order = 5)         -> a standard "average-performing" momentum advection scheme
 # default                 -> the best compromise between stability and accuracy. Very expensive
 
+elseif case == 6
+
 # Momentum advection benchmark
 @show trial1 = run_model_benchmark!(ocean_model_test, arch;
                                     use_benchmarktools = false,
                                     bottom_height = ocean_bathymetry())
+
+elseif case == 7
 
 # Simple advection benchmark
 @show trial2 = run_model_benchmark!(ocean_model_test, arch;
@@ -77,8 +95,12 @@ set_problem_size!(10, 10, 10)
                                     momentum_advection = VectorInvariant(),
                                     bottom_height = ocean_bathymetry())
 
+elseif case == 8
+
 @show trial3 = run_model_benchmark!(ocean_model_test, arch;
                                     use_benchmarktools = false,
                                     momentum_advection = VectorInvariant(),
                                     tracer_advection = Centered(),
                                     bottom_height = ocean_bathymetry())
+
+end
